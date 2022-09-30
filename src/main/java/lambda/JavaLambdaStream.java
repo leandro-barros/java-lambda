076ac5bfb1dfac0f5b78665684fef22fd38df28e
Leandro Barros <leandroedbarros@gmail.com>
2022-09-30 20:47:39 -0300

    feat: Example stream with filter

---
+++ src/main/java/lambda/JavaLambdaStream.java
@@ -0,0 +1,17 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JavaLambdaStream {
+
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        numbers.stream()
+                .filter(i -> i % 2 == 0)
+                .forEach(e -> System.out.println(e));
+    }
+
+
+}
