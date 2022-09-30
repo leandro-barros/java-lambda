4d1c54dbe4af583fb4659d0a5de50c93c690ead3
Leandro Barros <leandroedbarros@gmail.com>
2022-09-30 20:48:53 -0300

    feat: Comment stream

--- src/main/java/lambda/JavaLambdaStream.java
+++ src/main/java/lambda/JavaLambdaStream.java
@@ -8,6 +8,8 @@ public class JavaLambdaStream {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        // Strem: Fluxo de dados
         numbers.stream()
                 .filter(i -> i % 2 == 0)
                 .forEach(e -> System.out.println(e));
