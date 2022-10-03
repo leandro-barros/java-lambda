199d701605b9a6b64aa425967aa3b1ba65450f7e
Leandro Barros <leandroedbarros@gmail.com>
2022-10-03 19:46:29 -0300

    feat: Using limit and distict with stream

--- src/main/java/lambda/JavaLambdaStream.java
+++ src/main/java/lambda/JavaLambdaStream.java
@@ -7,15 +7,18 @@ public class JavaLambdaStream {
 
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 9);
 
 //        numbers.stream().forEach(i -> System.out.println(i));
 
         // Strem: Fluxo de dados
         numbers.stream()
-                .skip(2)
-                .filter(i -> i % 2 == 0)
+                .skip(2) // Operação intermediária
+                .limit(3)
+                .distinct()
+//                .filter(i -> i % 2 == 0)
                 .forEach(e -> System.out.println(e));
+
     }
 
 
