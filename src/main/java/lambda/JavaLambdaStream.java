f08ef822a4badb11787dd07496dd5a4e77317aae
Leandro Barros <leandroedbarros@gmail.com>
2022-10-03 19:42:51 -0300

    feat: Using skip stream

--- src/main/java/lambda/JavaLambdaStream.java
+++ src/main/java/lambda/JavaLambdaStream.java
@@ -7,12 +7,13 @@ public class JavaLambdaStream {
 
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-        numbers.stream().forEach(i -> System.out.println(i));
+//        numbers.stream().forEach(i -> System.out.println(i));
 
         // Strem: Fluxo de dados
         numbers.stream()
+                .skip(2)
                 .filter(i -> i % 2 == 0)
                 .forEach(e -> System.out.println(e));
     }
