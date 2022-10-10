36fe6c91756f6a2e55bc9744522569dd69aa9b94
Leandro Barros <leandroedbarros@gmail.com>
2022-10-10 19:49:10 -0300

    feat: Using orElseGet

--- src/main/java/optional/using/OptionalOrElse.java
+++ src/main/java/optional/using/OptionalOrElse.java
@@ -5,12 +5,16 @@ import java.util.Optional;
 public class OptionalOrElse {
 
     public static void main(String[] args) {
-        String number = "1d";
+        String number = "1";
 
+        // só retorna o 2 caso colocasse uma string para converter.
         Integer numberConverting = convertNumber(number).orElse(2);
 
-        System.out.println(numberConverting);
+        // só retorna o 5 caso colocasse uma string para converter.
+        Integer number2 = convertNumber(number).orElseGet(() -> 5);
 
+        System.out.println(numberConverting);
+        System.out.println(number2);
     }
 
     public static Optional<Integer> convertNumber(String value) {
