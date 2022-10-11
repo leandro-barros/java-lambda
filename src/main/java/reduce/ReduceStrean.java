3932dfc428c6f3dfc3d8d24d9959cfe3e6827b26
Leandro Barros <leandroedbarros@gmail.com>
2022-10-11 19:38:54 -0300

    docs: Comment about reduce

--- src/main/java/reduce/ReduceStrean.java
+++ src/main/java/reduce/ReduceStrean.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+// O reduce precisa ser associativo, ou seja, resolver em pequenos pedaços sempre irá dá o mesmo valor
 public class ReduceStrean {
 
     public static void main(String[] args) {
