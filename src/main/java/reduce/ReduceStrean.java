6f559cb8da6df5cded785555fc1981fd8f4b3868
Leandro Barros <leandroedbarros@gmail.com>
2022-10-11 19:27:10 -0300

    feat: Example reduce

---
+++ src/main/java/reduce/ReduceStrean.java
@@ -0,0 +1,18 @@
+package reduce;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class ReduceStrean {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2, 3, 4, 5, 6);
+
+        Optional<Integer>  reduce =  list.stream()
+                                        .reduce((n1, n2) -> n1 + n2);
+
+        System.out.println(reduce.get());
+    }
+
+}
