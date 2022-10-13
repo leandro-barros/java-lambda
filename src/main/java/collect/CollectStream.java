a765bcc7d4b5208fcffeca1f5d81952574269c2e
Leandro Barros <leandroedbarros@gmail.com>
2022-10-13 19:32:57 -0300

    feat: Starting collect with stream

---
+++ src/main/java/collect/CollectStream.java
@@ -0,0 +1,13 @@
+package collect;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectStream {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2, 3, 4, 5, 6);
+
+    }
+
+}
