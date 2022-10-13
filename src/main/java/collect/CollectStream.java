567a91f15005ec6831a6685c1601a6959b7f04cd
Leandro Barros <leandroedbarros@gmail.com>
2022-10-13 19:33:50 -0300

    feat: Colect conceit

--- src/main/java/collect/CollectStream.java
+++ src/main/java/collect/CollectStream.java
@@ -3,6 +3,7 @@ package collect;
 import java.util.Arrays;
 import java.util.List;
 
+// Utilizados para objetos mutáveis
 public class CollectStream {
 
     public static void main(String[] args) {
