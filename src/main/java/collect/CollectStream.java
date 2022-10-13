87b9cff9d6390c5dfe41113bd2c358cfafa94e05
Leandro Barros <leandroedbarros@gmail.com>
2022-10-13 19:40:00 -0300

    feat: Using colect

--- src/main/java/collect/CollectStream.java
+++ src/main/java/collect/CollectStream.java
@@ -1,5 +1,6 @@
 package collect;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,6 +9,14 @@ public class CollectStream {
 
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1,2, 3, 4, 5, 6);
+
+        // Fornecedor - acumulação - combinação
+        List<Object>  collect = list.stream()
+                                    .collect(() -> new ArrayList<Object>(),
+                                            (l, e) -> l.add(e),
+                                            (l1, l2) -> l1.addAll(l2));
+
+        System.out.println(collect);
 
     }
 
