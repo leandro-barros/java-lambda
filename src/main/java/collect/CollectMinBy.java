09dbdff46dd977c307b25b18ba7f2b869d9f930e
Leandro Barros <leandroedbarros@gmail.com>
2022-10-17 19:12:30 -0300

    feat: Collect minBy

---
+++ src/main/java/collect/CollectMinBy.java
@@ -0,0 +1,21 @@
+package collect;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Utilizados para objetos mutáveis
+public class CollectMinBy {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2, 3, 4, 5, 6);
+
+        list.stream()
+                .filter((e) -> e % 2 == 0)
+                .collect(Collectors.minBy(Comparator.naturalOrder()))
+                .ifPresent(System.out::println);
+
+    }
+
+}
