9672fd0286fcda6bca5c9f20e78ab12e300acd8a
Leandro Barros <leandroedbarros@gmail.com>
2022-10-18 19:46:59 -0300

    feat: Type arguments in lambida

---
+++ src/main/java/lambidaFormas/JavaLambda.java
@@ -0,0 +1,18 @@
+package lambidaFormas;
+
+import java.util.stream.IntStream;
+
+public class JavaLambda {
+
+    public static void main(String[] args) {
+        IntStream.range(0, 5)
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+
+        IntStream.range(0, 5)
+                .filter((int n) -> n % 2 == 0)
+                .reduce((int n1, int n2) -> n1 + n2)
+                .ifPresent(System.out::println);
+    }
+
+}
