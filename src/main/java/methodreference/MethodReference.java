43b829852cebdaf59eaab25c1718b54b079903d3
Leandro Barros <leandroedbarros@gmail.com>
2022-10-19 19:03:32 -0300

    feat: Starting method reference

---
+++ src/main/java/methodreference/MethodReference.java
@@ -0,0 +1,14 @@
+package methodreference;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MethodReference {
+
+    public static void main(String[] args) {
+
+        List<Integer> asList = Arrays.asList(1, 2, 3, 4, 5);
+
+    }
+
+}
