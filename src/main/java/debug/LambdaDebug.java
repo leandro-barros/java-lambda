a49ddc618080218e371d87c8fbf396b6b10a3e89
Leandro Barros <leandroedbarros@gmail.com>
2022-10-21 19:41:11 -0300

    feat: Example debug create StringBuilder

--- src/main/java/debug/LambdaDebug.java
+++ src/main/java/debug/LambdaDebug.java
@@ -1,8 +1,16 @@
 package debug;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LambdaDebug {
 
     public static void main(String[] args) {
+        List<Integer> asList = Arrays.asList(1, 2, 5, 8, 9);
+
+        asList.stream()
+                .map(n -> new StringBuilder().append(n).append("s").append("a"))
+                .forEach(System.out::println);
 
     }
 
