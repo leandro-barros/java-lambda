8fcf5cda2fcd1bef4cf08b71043a4bcb168917a1
Leandro Barros <leandroedbarros@gmail.com>
2022-10-22 16:29:28 -0300

    feat: Starting function interface'

---
+++ src/main/java/fuctionaliterface/FuctionInterface.java
@@ -0,0 +1,12 @@
+package fuctionaliterface;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FuctionInterface {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2, 3, 4, 5, 6);
+    }
+
+}
