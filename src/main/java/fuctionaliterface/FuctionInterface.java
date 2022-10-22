5408f8e9e8e5e97ec23ac18f975eedc9c531a4b6
Leandro Barros <leandroedbarros@gmail.com>
2022-10-22 16:39:28 -0300

    feat: Example suplier'

--- src/main/java/fuctionaliterface/FuctionInterface.java
+++ src/main/java/fuctionaliterface/FuctionInterface.java
@@ -2,11 +2,15 @@ package fuctionaliterface;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class FuctionInterface {
 
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1,2, 3, 4, 5, 6);
+
+        Stream.generate(() -> new java.util.Random().nextInt())
+                .forEach(System.out::println);
     }
 
 }
