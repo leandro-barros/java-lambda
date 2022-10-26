f565af8e0300e84bee0a087c5c2b3b0688e1b36f
Leandro Barros <leandroedbarros@gmail.com>
2022-10-25 21:19:57 -0300

    feat: Example binaryOperator

--- src/main/java/fuctionaliterface/UnaryOperator.java
+++ src/main/java/fuctionaliterface/UnaryOperator.java
@@ -1,9 +1,17 @@
 package fuctionaliterface;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class UnaryOperator {
 
     public static void main(String[] args) {
 
+        List<Integer> list = Arrays.asList(1,2, 3, 4, 5, 6);
+
+        list.stream()
+                .reduce((n1, n2) -> n1 + n2)
+                .ifPresent(System.out::println);
     }
 
 }
