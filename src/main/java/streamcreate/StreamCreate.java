b4ef4fefe231f07ce2e62a0f49b6c6a7a3cfd797
Leandro Barros <leandroedbarros@gmail.com>
2022-10-28 19:26:50 -0300

    feat: Example Stream with Stream of

--- src/main/java/streamcreate/StreamCreate.java
+++ src/main/java/streamcreate/StreamCreate.java
@@ -2,15 +2,21 @@ package streamcreate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamCreate {
 
     public static void main(String[] args) {
+        // List
         List<Integer> asList = Arrays.asList(1, 2, 3, 4);
         asList.stream().forEach(System.out::println);
 
+        // Array
         Integer[] array = new Integer[] {1, 2, 3};
         Arrays.stream(array).forEach(System.out::println);
+
+        Stream.of("Value one", "Value two", "Value three")
+                .forEach(System.out::println);
 
     }
 
