d405cb374086cd45c4341980cd50561f33d2d6c0
Leandro Barros <leandroedbarros@gmail.com>
2022-10-28 19:24:15 -0300

    feat: Example Stream with Arrays

--- src/main/java/streamcreate/StreamCreate.java
+++ src/main/java/streamcreate/StreamCreate.java
@@ -9,6 +9,9 @@ public class StreamCreate {
         List<Integer> asList = Arrays.asList(1, 2, 3, 4);
         asList.stream().forEach(System.out::println);
 
+        Integer[] array = new Integer[] {1, 2, 3};
+        Arrays.stream(array).forEach(System.out::println);
+
     }
 
 }
